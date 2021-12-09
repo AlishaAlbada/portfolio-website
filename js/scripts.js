fda42d1afe3298318b82d88d8ea81dae5c52fef2
AlishaAlbada <alisha.albada@gmail.com>
2021-12-08 17:06:05 -0700

    2.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 function menuToggle() {
   var x = document.getElementById('myNavToggle');
   if (x.className === 'navtoggle') {
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
-}
-}
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle';
